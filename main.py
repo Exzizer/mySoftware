5636c3f4a306648215c41e24126daa1b52c8454a
mike <michael.p9392@gmail.com>
2022-07-09 21:24:00 +0300

    Screen commit

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def click():
-    print("Click")
+def welcome():
+    print("New Screen")
 
 
 # Press the green button in the gutter to run the script.
